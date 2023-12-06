7edea4caab461221120658bb404ef420ae9f32ed
StivenArnaud <stivenarnaud1@gmail.com>
2023-12-06 17:18:25 +0000

    Front end modifier

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def chatbot():
-    return render_template('chatbot.html')
+
+    question = ''
+    if request.method == "POST":
+        question = request.form.get('data')
+        print(question)
+    return render_template('chatbot.html', reponse=question)
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
+
+
     
