7cc6bca28f494cf92d7fd9cc6a6f4d761a5d09d4
attacheaka <attacheaka66@gmail.com>
2023-12-15 17:05:58 +0000

    Maj

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 
-from flask import Flask, render_template , request, jsonify, make_response ,  redirect, url_for
+from flask import Flask, render_template , request, jsonify, make_response ,  redirect, url_for , session
 from appmanager.controllers.search import process_query
 from dotenv import load_dotenv
 import os
-from appmanager.config.var_config import LIST_MODULE_IA
+from appmanager.config.var_config import LIST_MODULE_IA , SESSION_OPTION_MODULE
 
 # Chargement des envs
 load_dotenv()
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = os.getenv('APP_SECRET_KEY')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def chatbot():
+    # Listing des options 
+    options_module = LIST_MODULE_IA
+    # Recuperation des sessions 
+    if(session.get(SESSION_OPTION_MODULE) == None):
+        session[SESSION_OPTION_MODULE] = LIST_MODULE_IA[0]     
     result = ''
     if request.method == "POST":
         question = request.form.get('question')
@@ -20,7 +26,10 @@ def chatbot():
         # data = request.get_json()
         # query = data.get('question')
         result = process_query(question)
-    return render_template('chatbot.html', response=result)
+    return render_template('chatbot.html', response=result , 
+        session_option = session.get(SESSION_OPTION_MODULE),
+        options_module = options_module
+    )
 
 
 # Pour choisir le type de module de l'iA
@@ -28,12 +37,12 @@ def chatbot():
 @app.route('/type_module', methods=['POST'])
 def type_module():
     data = request.form
-    query = data.get('option')
+    query = data.get('options')
     if query not in LIST_MODULE_IA:
         query = LIST_MODULE_IA[0]
 
     # Sauvearder dans la session
-        
+    session[SESSION_OPTION_MODULE] = query
     # Redirection index
     return redirect(url_for('chatbot'))
    
--- appmanager/config/var_config.py
+++ appmanager/config/var_config.py
@@ -2,6 +2,9 @@
 Liste des constantes 
 """
 
+# Liste des sessions
+SESSION_OPTION_MODULE = "option_module"
+
 # liste de module 
 LIST_MODULE_IA = ["standard", "analyste"]
 
